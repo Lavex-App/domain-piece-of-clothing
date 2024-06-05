e2abc78b588c677f996c2ddd0643a12a3c404278
MrAbade <vini.abade@outlook.com.br>
2024-06-04 22:54:48 -0300

    feature(#18): add Cloth Specification Adapter with the method add_all on adapters layer
    feature-create-new-specification-and-add-it-to-a-specific-piece-of-clothing

--- domain_piece_of_clothing/adapters/interface_adapters/__init__.py
+++ domain_piece_of_clothing/adapters/interface_adapters/__init__.py
@@ -1,6 +1,9 @@
+from .cloth_specification_adapter import ClothSpecificationAdapter, ClothSpecificationProviders
 from .piece_of_clothing_adapter import PieceOfClothingAdapter, PieceOfClothingProviders
 
 __all__ = [
     "PieceOfClothingProviders",
     "PieceOfClothingAdapter",
+    "ClothSpecificationAdapter",
+    "ClothSpecificationProviders",
 ]
---
+++ domain_piece_of_clothing/adapters/interface_adapters/cloth_specification_adapter.py
@@ -0,0 +1,45 @@
+from typing import NamedTuple
+
+from bson import ObjectId
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
+
+from domain_piece_of_clothing.business.services import ClothSpecificationService
+from domain_piece_of_clothing.models import ClothSpecificationIdModel, ClothSpecificationModel
+
+from .exceptions import DocumentIdNotFoundException, DatabaseOperationNotAllowedException
+from .interfaces import DatabaseName, DocumentDatabaseProvider, InterfaceAdapter
+
+ProviderType = DocumentDatabaseProvider[AsyncIOMotorClient, AsyncIOMotorDatabase]
+
+
+class ClothSpecificationProviders(NamedTuple):
+    document_database_provider: ProviderType
+
+
+class ClothSpecificationAdapter(InterfaceAdapter, ClothSpecificationService):
+    def __init__(self, providers: ClothSpecificationProviders) -> None:
+        database_provider = providers.document_database_provider
+        database_provider.database = DatabaseName.PIECE_OF_CLOTHING  # type: ignore
+        self.__cloths_collection = database_provider.database["clothes"]
+
+    async def add_all(
+        self,
+        piece_of_clothing_id: str,
+        cloth_specifications: list[ClothSpecificationModel],
+    ) -> list[ClothSpecificationIdModel]:
+        update_filter = {"_id": ObjectId(piece_of_clothing_id)}
+        found_piece_of_clothing = await self.__cloths_collection.find_one(update_filter)
+        if not found_piece_of_clothing:
+            raise DocumentIdNotFoundException()
+        max_specification_id = max((specification["id"] for specification in found_piece_of_clothing["specifications"]))
+        new_specifications = [
+            {"id": str(cloth_specification_id), **cloth_specification.model_dump()}
+            for cloth_specification_id, cloth_specification in enumerate(
+                cloth_specifications, start=int(max_specification_id) + 1
+            )
+        ]
+        insertions = {"$push": {"specifications": {"$each": new_specifications, "$sort": {"id": 1}}}}
+        result = await self.__cloths_collection.update_one(update_filter, insertions)
+        if not result.modified_count:
+            raise DatabaseOperationNotAllowedException()
+        return [ClothSpecificationIdModel(**specification) for specification in new_specifications]
