7efc90a71e0743aae30f7912f2dd7a604ea3b82b
MrAbade <vini.abade@outlook.com.br>
2024-06-04 22:56:36 -0300

    feature(#18): add POST HTTP method to add a Cloth Specification by the Piece of Clothing ID to the new controller cloth_specification_controller
    feature-create-new-specification-and-add-it-to-a-specific-piece-of-clothing

--- domain_piece_of_clothing/adapters/controllers/__binding__.py
+++ domain_piece_of_clothing/adapters/controllers/__binding__.py
@@ -1,8 +1,10 @@
 from fastapi.applications import FastAPI
 
 from .piece_of_clothing_controller import piece_of_clothing_controller
+from .cloth_specification_controller import cloth_specification_controller
 
 
 class Binding:
     def register_all(self, app: FastAPI) -> None:
         app.include_router(piece_of_clothing_controller)
+        app.include_router(cloth_specification_controller)
